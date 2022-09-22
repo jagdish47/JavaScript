5ec9b7b7d84d36189507c16fae084e300e4499d7
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-09-22 17:32:39 +0530

    basic of javascript

---
+++ JavaScript DSA Problems/Basic.js
@@ -0,0 +1,12 @@
+// this will give you squre of number
+console.log(2**2);
+console.log(2**3);
+
+// this will give you squre root of number
+console.log(Math.floor(16**0.5));
+
+// this will give you reminder
+console.log(5 % 2);
+console.log(5 % 3);
+console.log(5 % 1);
+console.log(5 % 5);
