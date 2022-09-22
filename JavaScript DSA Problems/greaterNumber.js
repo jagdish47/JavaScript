9373daf720368c90dbe33c2a3afcc8f9652b4670
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-09-22 17:35:33 +0530

    greater number

--- JavaScript DSA Problems/greaterNumber.js
+++ JavaScript DSA Problems/greaterNumber.js
@@ -1,4 +1,13 @@
 var a = 10;
 var b = 20; 
-var c = 30;
+var c = 20;
 
+if(a >= b && a >= c){
+    console.log("a is greater");
+}
+else if(b >= a && b >= c){
+    console.log("b is greater");
+}
+else{
+    console.log("c is greater");
+}
