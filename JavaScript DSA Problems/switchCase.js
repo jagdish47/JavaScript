e5adf9c630e3944d762a929a5a01fef28f904825
Jagdish Kumawat <jagdishkumawat81@gmail.com>
2022-09-22 17:43:26 +0530

    revice switch case

---
+++ JavaScript DSA Problems/switchCase.js
@@ -0,0 +1,38 @@
+var day = 8;
+
+if(day > 7){
+    day = day % 7;
+}
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("Wednesday");
+    break;
+
+  case 4:
+    console.log("Thursday");
+    break;
+
+  case 5:
+    console.log("Friday");
+    break;
+
+  case 6:
+    console.log("Saturday");
+    break;
+
+  case 7:
+    console.log("Sunday");
+    break;
+
+  default:
+    console.log("Please! Enter between 1 to 7")
+}
