1ba89c43e8d039082759513be9074becc153f203
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-29 20:29:10 +0530

    basic of string

---
+++ Revision/String/one.js
@@ -0,0 +1,8 @@
+let name = "Jagdish Kumawat";
+
+// console.log(name);
+
+for (let i = 0; i < name.length; i++) {
+  console.log(i); // it will go till last charactaer in string
+  console.log(name[i]);
+}
