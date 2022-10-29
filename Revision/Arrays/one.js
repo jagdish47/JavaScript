492c35a540994a7f8be4563eda2c353633a0e271
jagdishkumawat <jagdishkumawat81@gmail.com>
2022-10-29 20:20:49 +0530

    basic of array

---
+++ Revision/Arrays/one.js
@@ -0,0 +1,9 @@
+let arr = [10, 20, 30, 40, 50];
+
+console.log(arr.indexOf(10)); //give index of first occurance of value if not present it will give you -1
+console.log(arr.lastIndexOf(10)); //give last occurance of index if not present it will give you -1
+console.log(arr.length); //it will give you complete length of array
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
